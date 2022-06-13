ed826ea1578ecd8abc0ef0f101371c5fde0abd17
Sk Anisuddin Ahmed <57311176+anisuddin1@users.noreply.github.com>
2022-06-13 22:03:18 +0530

    Update m2-t2-e7.cpp

--- m2-diving-into-c-programming/m2-code challenges - solution/m2-t2-e7.cpp
+++ m2-diving-into-c-programming/m2-code challenges - solution/m2-t2-e7.cpp
@@ -1,49 +1,20 @@
 #include <stdio.h>
-
-
-int main() {
-
-    int num, reversedNumber = 0, remainder;
-
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    // Store user input in a variable so that the reversedNumber can
-    // be compared to the original user input in the end.
-    int originalNum = num;
-
-    do {
-        remainder = num % 10;
-        reversedNumber = (reversedNumber * 10) + remainder;
-        num = num / 10;
-    } while (num > 0);
-
-    if (originalNum == reversedNumber)
-        printf("Palindrome");
-    else
-        printf("Not a palindrome");
-
-
-    return 0;
+#include <string.h>
+#include <stdlib.h>
+void main(){
+  printf("Enter the number to check:");
+  int num;
+  scanf("%d", &num);
+  char cnum[10];
+  char rcnum[10];
+  sprintf(cnum,"%d",num);
+  strcpy(rcnum,cnum);
+  strrev(rcnum);
+  int c1 = atoi(cnum);
+  int c2 = atoi(rcnum);
+  if (c1 == c2){
+    printf("it is a palindrome");
+  } else {
+    printf("it is not a palindrome");
+  }
 }
-
-/* Explanation: 
-Assume num  = 121
-originalNum = num = 121
-Do while loop will execute until num is greater than 0 
-1st iteration:
-remainder = num % 10 = 121 % 10 = 1 
-reversedNumber = (reversedNumber * 10) + remainder = 0 * 10 + 1 = 1
-num = num / 10 = 121 / 10 = 12
-2st iteration:
-remainder = num % 10 = 12 % 10 = 2
-reversedNumber = (reversedNumber * 10) + remainder = 1 * 10 + 2 = 12
-num = num / 10 = 12 / 10 = 1
-3rd iteration:
-remainder = num % 10 = 1 % 10 = 1
-reversedNumber = (reversedNumber * 10) + remainder = 12 * 10 + 1 = 121
-num = num / 10 = 1 / 10 = 0 
-Loop terminates since num becomes 0 at the end of 3rd iteration. 
-Since reversedNum and originalNum are found to be equal, therefore the entered number is a palindrome. 
-*/
-
